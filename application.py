1e3ef3e5cc65cf47086a4d817cd1af9f9053b505
JordanSil <jcsilver87@gmail.com>
2025-01-20 05:20:29 +0200

    remove # from app run for heroku

--- application.py
+++ application.py
@@ -471,5 +471,5 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-#if __name__ == "__main__":
-#    application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
